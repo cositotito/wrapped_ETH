6f754cd3f155b75a9c4966693b02cfe6d8babb68
cositotito <52358699+cositotito@users.noreply.github.com>
2022-02-09 16:14:19 -0500

    Update WETH_convert.sol

--- WETH_convert.sol
+++ WETH_convert.sol
@@ -1,16 +1,20 @@
-pragma solidity >=0.6.0;
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.1;
 
 interface WethLike {
     function deposit() external payable;
     function withdraw(uint256) external;
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
+    // function withdrawTo(address payable to) external;
+    function balanceOf(address) external view returns (uint);
 }
 
 contract MyContract {
   WethLike weth;
   address tokenWETH= 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
-  constructor() public{
+  constructor() {
     weth = WethLike(tokenWETH); // Kovan
     weth.approve(address(this),1000000000000000000000000);
      weth.approve(tokenWETH,1000000000000000000000000);
@@ -20,17 +24,25 @@ contract MyContract {
   function deposit(uint256 _amount) external payable {
     require(_amount==msg.value,"amount -1");
     emit Data(msg.value);
-
   }
 
-  event DepositETH(uint256);
-  function depositWETH() external payable {
-   
+  event DepositWETH(uint256);
+  function depositWETH() external {   
     weth.deposit{ value: 1 }();
-    emit DepositETH(1);
+    emit DepositWETH(1);
   }
 
-  function withdraw(uint256 _amount) public { 
-    weth.withdraw(_amount);    
+
+  function balanceOfContract() public view returns(uint256){
+    return weth.balanceOf(address(this));
+  }
+
+  function withdrawTo(address payable to) public payable  { 
+    uint balance= weth.balanceOf(address(this));
+    if (balance > 0) {
+      weth.withdraw(balance);    
+      (bool success,) = to.call{value: balance, gas:100000}("");
+      require(success, "WITHDRAW_TO_CALL_FAILED");
+    }    
   }
 }
